39a701269f1f2f53f98b259db67ace7e744e5b93
Tony Pujals <tony@subfuzion.com>
2018-04-05 22:50:22 -0700

    improve rotate_matrix

--- chapter-01/rotate_matrix_test.go
+++ chapter-01/rotate_matrix_test.go
@@ -65,6 +65,24 @@ func TestRotateMatrix(t *testing.T) {
 				{8, 5, 2},
 			},
 		},
+		sample{
+			Input: matrix{
+				{0, 1, 2, 3, 4, 5},
+				{6, 7, 8, 9, 10, 11},
+				{12, 13, 14, 15, 16, 17},
+				{18, 19, 20, 21, 22, 23},
+				{24, 25, 26, 27, 28, 29},
+				{30, 31, 32, 33, 34, 35},
+			},
+			Expected: matrix{
+				{30, 24, 18, 12, 6, 0},
+				{31, 25, 19, 13, 7, 1},
+				{32, 26, 20, 14, 8, 2},
+				{33, 27, 21, 15, 9, 3},
+				{34, 28, 22, 16, 10, 4},
+				{35, 29, 23, 17, 11, 5},
+			},
+		},
 	}
 
 	for _, samp := range tests {
@@ -91,9 +109,10 @@ func rotate(m matrix) bool {
 
 	// work our way layer by layer toward the middle
 	n := len(m)
-	for layer := 0; layer < n; layer++ {
-		// this is a square matrix, so first and last identify starting and ending cell
-		// for row of each layer
+	for layer := 0; layer < n/2; layer++ {
+		// this is a square matrix, so first and last identify starting and ending
+		// boundaries of layer; ex (clockwise from top left):
+		// [first][first], [first][last], [last][last], [last][first]
 		// Ex: 6x6 matrix
 		// iter 0:  first=0 last=5
 		// iter 1:  first=1 last=4
@@ -102,9 +121,20 @@ func rotate(m matrix) bool {
 		last := n - 1 - layer
 
 		for i := first; i < last; i++ {
+			// As we advance, i represents the current column on the top row and
+			// the current row on the right side. Because we are rotating, the
+			// bottom and left sides advance in a negative direction, so we need
+			// a zero-based offset to compute corresponding locations from the ends.
+			// For every rotation, the cells that being shifted are identified as:
+			// (given the boundaries: [first][first], [last][last])
+			// top side:    [first][i] (advances right)
+			// left side:   [last-offset][first] (advances up)
+			// bottom side: [last][last-offset] (advances left)
+			// right side:  [i][last] (advances down)
 			offset := i - first
 
-			// save before overwriting
+			// save layer top cell before overwriting with corresponding cell from left side
+			// assume saving layer top row cells first, from first to last for each i
 			top := m[first][i]
 
 			// move left to top
