59cd594c8b86921e3c95dae81159ebe293959eb0
Tony Pujals <tony@subfuzion.com>
2018-04-09 00:22:12 -0700

    add bitset to 101_is_unique

--- chapter-01/101_is_unique_test.go
+++ chapter-01/101_is_unique_test.go
@@ -9,17 +9,27 @@ func TestUniqueRunes(t *testing.T) {
 		"a":           true,
 		"ab":          true,
 		"abcdefghijk": true,
-		"abc123@=%^":  true,
+		"abcxyz":      true,
 		"aa":          false,
 		"abcc":        false,
 		"abccde":      false,
 	}
 
-	for key, expected := range tests {
-		if actual := IsUnique(key); actual != expected {
-			t.Errorf("Expected unique=%t for %s", expected, key)
+	t.Run("using_map", func(t *testing.T) {
+		for key, expected := range tests {
+			if actual := IsUnique(key); actual != expected {
+				t.Errorf("Expected unique=%t for %s", expected, key)
+			}
 		}
-	}
+	})
+
+	t.Run("using_bitset", func(t *testing.T) {
+		for key, expected := range tests {
+			if actual := IsUniqueUsingBitset(key); actual != expected {
+				t.Errorf("Expected unique=%t for %s", expected, key)
+			}
+		}
+	})
 }
 
 //
@@ -48,6 +58,20 @@ func IsUnique(s string) bool {
 		if ok := set.Add(r); !ok {
 			return false
 		}
+	}
+	return true
+}
+
+// assumes only ascii
+func IsUniqueUsingBitset(s string) bool {
+	set := 0
+	for _, ch := range []rune(s) {
+		val := ch - 'a'
+		mask := 1 << uint32(val)
+		if set&mask > 0 {
+			return false
+		}
+		set |= mask
 	}
 	return true
 }
