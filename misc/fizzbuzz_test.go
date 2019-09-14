014d613ec5a31cbfa475876779bd59e6b2da2c86
Tony Pujals <tpujals@gmail.com>
2019-09-14 01:34:21 -0700

    improve fizzbuzz

--- misc/fizzbuzz_test.go
+++ misc/fizzbuzz_test.go
@@ -1,3 +1,35 @@
+/*
+
+FizzBuzz
+
+fizzbuzz prints to a string all digits from 1 to n so that:
+- for every 3rd digit, print "Fizz"
+- for every 5 digit, print "Buzz
+- for every multiple of 3 and 5, print "FizzBuzz"
+
+Example:
+  for n = 15
+  result = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz"
+			1
+			 2
+			  3 print "Fizz"
+				  4
+				   5 print "Buzz"
+					   6 print "Fizz" (multiple of 3)
+						   7
+							8
+							 9 print "Fizz" (multiple of 3)
+								 10 print "Buzz" (multiple of 5)
+									 11
+									   12 print "Fizz" (multiple of 3)
+										   13
+											 14
+											   15 print "FizzBuzz"
+											      (multiple of 3 and 5)
+
+
+*/
+
 package misc
 
 import (
@@ -5,6 +37,9 @@ import (
 	"strings"
 	"testing"
 )
+
+// FizzBuzzFunc declares a type of FizzBuzz function
+type FizzBuzzFunc func(int) string
 
 func TestFizzBuzz(t *testing.T) {
 
@@ -20,14 +55,18 @@ func TestFizzBuzz(t *testing.T) {
 		},
 	}
 
-	for _, samp := range tests {
-		actual := fizzbuzz(samp.n)
-		expected := samp.expected
+	test := func(t *testing.T, name string, f FizzBuzzFunc) {
+		for _, samp := range tests {
+			actual := f(samp.n)
+			expected := samp.expected
 
-		if actual != expected {
-			t.Errorf("\nfor n=%d, expected: %s, actual: %s", samp.n, expected, actual)
+			if actual != expected {
+				t.Errorf("\nfor n=%d, expected: %s, actual: %s", samp.n, expected, actual)
+			}
 		}
 	}
+
+	test(t, "fizzbuzz", fizzbuzz)
 }
 
 // fizzbuzz prints to a string all digits from 1 to n
